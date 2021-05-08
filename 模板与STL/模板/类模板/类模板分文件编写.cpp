7a0d74da5c082c785d5496fef1447c03ea239e3b
blue-arch <55428332+blue-arch@users.noreply.github.com>
2021-05-08 21:34:22 +0800

    终于到STL了

---
+++ 模板与STL/模板/类模板/类模板分文件编写.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<typeinfo>
+using namespace std;
+
+#include<iostream>
+using namespace std;
+
+//#include "person.h"
+#include "person.hpp" //解决方式1，包含cpp源文件
+
+//解决方式2，将声明和实现写到一起，文件后缀名改为.hpp
+#include "person.hpp"
+void test01()
+{
+	Person<string, int> p("Tom", 10);
+	p.showPerson();
+}
+
+int main() {
+
+	test01();
+
+	system("pause");
+
+	return 0;
+}
