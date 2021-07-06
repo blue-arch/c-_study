517a18931c93d27b28af5f81fb53131b8cb1df6e
blue-arch <55428332+blue-arch@users.noreply.github.com>
2021-07-06 18:14:30 +0800

    基础配置完成了

---
+++ testclion/day01/test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by 17208 on 2021/7/6.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "hello world" << endl;
+}
+
---
+++ testclion/day02/test2.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 17208 on 2021/7/6.
+//
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "gergher" << endl;
+}
